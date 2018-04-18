410a79e8e55d2f9dfc2a3251bc6aaa9982a9faf4
Yurii Fediv <alpachino48@gmail.com>
2018-04-18 11:20:04 +0300

    Added comment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import { Rule, FieldsDescription } from './types';
 
 // Represents Public API of library, every method presented there
 // may be used by user and should be described in README file
+//
+// Separating of Public Api and Validator gives next benefits:
+//  1) User of library have access only to Public API but not to implementation details
+//  2) At the same time inner details of Validator are easy testable
+//  3) Also small benefit: in Validator we declare methods on prototype, but not on actual function -
+//     this is useful for reducing of initial render time,
+//     if project have a lot of forms(therefore a lot of instances of Validator object)
 function ValidationPublicApi(fields: FieldsDescription) {
   const validator = new Validator(fields);
 
