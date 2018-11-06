50e8db770ec750ccc4377cda37f06529482001ca
Ivanov Igor <i.ivanov@nullgr.com>
2018-11-06 17:31:59 +0200

    TESTS

--- __tests__/findDifference.test.js
+++ __tests__/findDifference.test.js
@@ -18,73 +18,61 @@ const validationState = {
   }
 };
 
-const wrongValidationState = {
-  login: {
-    value: '',
-    showError: false,
-    statuses: [false]
-  },
-  email: {
-    value: '',
-    showError: false,
-    statuses: [false]
-  },
-  password: {
-    value: '',
-    showError: false,
-    statuses: [false]
-  }
-};
-
-const nextState = {
-  email: 'oh god was changed',
-  password: ''
-};
-
-const superNextState = {
-  email: 'oh god was changed',
-  password: 'OH NO ME TOO'
-};
-
-const expectedNextDifference = {
-  email: {
-    value: 'oh god was changed',
-    showError: true,
-    statuses: [false]
-  }
-};
-
-const expectedSuperNextDifference = {
-  email: {
-    value: 'oh god was changed',
-    showError: true,
-    statuses: [false]
-  },
-  password: {
-    value: 'OH NO ME TOO',
-    showError: true,
-    statuses: [false]
-  }
-};
-
 describe('Unit tests for inner implementation of findDifference module', () => {
   test(`Testing initial state run`, () => {
     const difference = findDifference(initialState, validationState);
     expect(difference).toEqual({});
   });
 
-  test(`Testing next state run`, () => {
+  test(`Testing one field state change run`, () => {
+    const nextState = {
+      email: 'oh god was changed',
+      password: ''
+    };
+
+    const expectedNextDifference = {
+      email: {
+        value: 'oh god was changed',
+        showError: true,
+        statuses: [false]
+      }
+    };
+
     const difference = findDifference(nextState, validationState);
     expect(difference).toEqual(expectedNextDifference);
   });
 
-  test(`Testing SUPER next state run`, () => {
-    const difference = findDifference(superNextState, validationState);
-    expect(difference).toEqual(expectedSuperNextDifference);
+  test(`Testing two fields state change run`, () => {
+    const nextState = {
+      email: 'oh god was changed',
+      password: 'OH NO ME TOO'
+    };
+
+    const expectedNextDifference = {
+      email: {
+        value: 'oh god was changed',
+        showError: true,
+        statuses: [false]
+      },
+      password: {
+        value: 'OH NO ME TOO',
+        showError: true,
+        statuses: [false]
+      }
+    };
+
+    const difference = findDifference(nextState, validationState);
+    expect(difference).toEqual(expectedNextDifference);
   });
 
-  test(`Testing wrong initial state run`, () => {
-    const difference = findDifference(initialState, wrongValidationState);
+  test(`Testing extended state run`, () => {
+    const extendedState = {
+      email: '',
+      password: '',
+      message: 'i`m new here, whats up?'
+    };
+
+    const difference = findDifference(extendedState, validationState);
     expect(difference).toEqual({});
   });
 });
