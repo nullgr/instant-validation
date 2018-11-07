86ee7ea81e347ae1a1fb52c6f7e2b0e216101d9a
Ivanov Igor <i.ivanov@nullgr.com>
2018-11-07 14:29:11 +0200

    cleaner public api

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import Validator from './validator';
 import { FieldsDescription, ValidateReturn } from './types';
 
 interface ValidationPublicApi<State> {
-  setInitialValues: (state: State) => State;
   validate(state: State): ValidateReturn;
   isFormValid(): boolean;
 }
@@ -22,10 +21,6 @@ const ValidationPublicApi = (function<State>(
   fields: FieldsDescription
 ) {
   const validator = new Validator<State>(fields);
-
-  this.setInitialValues = function(state) {
-    return validator.setInitialValues(state);
-  };
 
   this.validate = function(state) {
     return validator.validate(state);
