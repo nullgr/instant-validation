b743ba18b2c53de14275b0226d41639ac30dbb58
Test <https://bitbucket.org/teamtrovvit/>
2019-08-23 16:10:45 +0300

    added min and max rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,3 +1,16 @@
 export const requiredRule = (value: string | boolean): boolean => !!value;
 export const lengthRule = (l: number) => (v: string): boolean =>
   !!v && v.length >= l;
+
+export const minLengthRule: (l: number) => (v: string) => boolean = function (l) {
+  return function (v) {
+    return !!v && v.length >= l;
+  };
+};
+
+
+export const maxLengthRule: (l: number) => (v: string) => boolean = function (l) {
+  return function (v) {
+    return !!v && v.length <= l;
+  };
+};
