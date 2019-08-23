3f90600e4c531bf9873ea5770406abfabdd88df1
Oleksandr Chernenko <akazimirkas@gmail.com>
2019-08-23 16:25:15 +0300

    Release 1.0.2: Added min and max rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,16 +1,20 @@
 export const requiredRule = (value: string | boolean): boolean => !!value;
+
 export const lengthRule = (l: number) => (v: string): boolean =>
   !!v && v.length >= l;
 
-export const minLengthRule: (l: number) => (v: string) => boolean = function (l) {
-  return function (v) {
+export const minLengthRule: (l: number) => (v: string) => boolean = function(
+  l
+) {
+  return function(v) {
     return !!v && v.length >= l;
   };
 };
 
-
-export const maxLengthRule: (l: number) => (v: string) => boolean = function (l) {
-  return function (v) {
+export const maxLengthRule: (l: number) => (v: string) => boolean = function(
+  l
+) {
+  return function(v) {
     return !!v && v.length <= l;
   };
 };
